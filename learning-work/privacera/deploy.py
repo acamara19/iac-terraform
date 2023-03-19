404d3922e512cb63afe6e1bce22986df8a53fa39
Ansoumane Camara <26487966+acamara19@users.noreply.github.com>
2023-03-19 13:51:46 -0400

    update deploy script

--- learning-work/privacera/deploy.py
+++ learning-work/privacera/deploy.py
@@ -1,8 +1,13 @@
 import os
 import subprocess
 
-def run_terraform_command(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def run_terraform_command(command, working_directory=None):
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=working_directory
+    )
     stdout, stderr = process.communicate()
 
     if process.returncode != 0:
@@ -15,8 +20,12 @@ def main():
     profile_name = "saml"
     os.environ["TF_VAR_AWS_PROFILE"] = profile_name
 
-    run_terraform_command(["terraform", "init"])
-    run_terraform_command(["terraform", "apply", "-auto-approve"])
+    # Get the root directory
+    root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    # Initialize and apply Terraform
+    run_terraform_command(["terraform", "init"], working_directory=root_directory)
+    run_terraform_command(["terraform", "apply", "-auto-approve"], working_directory=root_directory)
 
 if __name__ == "__main__":
     main()
