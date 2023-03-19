eb76cd6296269afd377a80a528af9642e56b70ae
Ansoumane Camara <26487966+acamara19@users.noreply.github.com>
2023-03-19 16:45:31 -0400

    updated variables

--- learning-work/privacera/privacera_services/variables.tf
+++ learning-work/privacera/privacera_services/variables.tf
@@ -27,3 +27,38 @@ variable "s3_bucket_name" {
   type        = string
   description = "Name of the S3 bucket where the files will be uploaded"
 }
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS profile to be used for authentication"
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket to be used for file upload and backend storage"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The ID of the VPC for which the endpoints and security group will be created"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR of the VPC for which the security group will be created"
+}
+
+variable "files_to_upload" {
+  type        = map(list(string))
+  description = "A map of subfolder names and the list of file names to be uploaded to the S3 bucket"
+}
+
+variable "external_account_id" {
+  type        = string
+  description = "The AWS account ID of the external account for the custom assume role policy"
+}
+
+variable "external_role_name" {
+  type        = string
+  description = "The name of the IAM role from the different account for the custom assume role policy"
+}
